6df4136c320e727151bd5f8d69b521315ea58a39
Konstantin Berezko <berezkok72@gmail.com>
2021-08-18 21:05:57 +0500

    new(K&K):create test task

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Getting Started with Create React App
+# Тестовое задание K&K Accelerator
+
+2 варианта:
+* React App
+* Vanilla JS /docs
+
+[Demo](https://kk-test-aca04.web.app)
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
 
 function App() {
+
+  const btn2Ref = useRef(null),
+    btn3Ref = useRef(null);
+
+  const [btn1Val, setBtn1Val] = useState(1),
+    [btn2State, setBtn2State] = useState('Hide'),
+    [btn3State, setBtn3State] = useState('Hide');
+
+  const showBtn = () => {
+    setBtn2State('');
+    setBtn3State('');
+    setBtn1Val(1);
+  };
+
+  const showHidden = e => {
+    const target = +e.current.value;
+    setBtn1Val(target);
+    (target === 2) ? setBtn2State('Hide') : setBtn3State('Hide');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "Wrap" >
+      <button onClick={showBtn} value={btn1Val}>{btn1Val}</button>
+      <button ref={btn2Ref} className={btn2State} onClick={() => showHidden(btn2Ref)} value="2">2</button>
+      <button ref={btn3Ref} className={btn3State} onClick={() => showHidden(btn3Ref)} value="3">3</button>
     </div>
   );
 }
